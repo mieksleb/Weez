b6dab5484453a4a740eb1567fef8b481dbf312b2
Rumee Ahmed <rumeeahmad@gmail.com>
2021-05-20 00:06:02 +0100

    Weezington Silva now has the ability to launch Chrome, go to Warzone and bypass the initial GDPR page.

--- weez_scraper.py
+++ weez_scraper.py
@@ -9,14 +9,48 @@ import time
 
 
 class WeezScraper(webdriver.Chrome):
-
+    """
+    Object that will scrape daily Warzone data for a given player.
+    """
+    # options = Options()
+    # options.headless = True
     _driver_path = '/Users/rumeeahmed/Documents/Weez/chromedriver'
+    _url = 'https://cod.tracker.gg/warzone'
 
     def __init__(self, username: str, platform: str):
+        """
+
+        :param username: The username for the player in question.
+        :param platform: The platform the username is associated with. For now only use use usernames that belong to
+        the PlayStation Network or the Activision Network.
+        """
         super().__init__(self._driver_path)
         self.username = username
         self.platform = platform
-        self.executable_path = self._driver_path
+        self.make_request()
+
+    def make_request(self):
+        """
+        Perform a get request on the
+        :return:
+        """
+        self.get(self._url)
+        self._bypass_gpdr()
+
+    def _bypass_gpdr(self):
+        """
+        The first page is often a GDPR policy page, this method will click and then suppress the page.
+        :return:
+        """
+
+        try:
+            button = WebDriverWait(self, 10).until(
+                EC.presence_of_element_located((By.ID, 'onetrust-accept-btn-handler'))
+            )
+            button.click()
+
+        except NoSuchElementException:
+            pass
 
 
 scraper = WeezScraper('RumeeAhmed', 'PS4')
