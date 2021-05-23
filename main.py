a1452cd974b13d79e41cf4df26b6a8cb3cad3229
Rumee Ahmed <rumeeahmad@gmail.com>
2021-05-23 20:00:52 +0100

    Initial commit

---
+++ main.py
@@ -0,0 +1,7 @@
+from Scraper.weez_scraper import WeezScraper
+# from GNBot.gn_bot import GNBot
+
+rumee = WeezScraper('RumeeAhmed', 'PS')
+rumee.scrape()
+print(rumee.overall_stats)
+print(rumee.match_stats)
