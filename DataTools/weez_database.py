2ac63226be9fe21f6a01059e7ddf0803e38ce3e6
Rumee Ahmed <rumeeahmad@gmail.com>
2021-07-28 19:56:06 +0100

    This now writes to Friestore instead of realtime database.

--- DataTools/weez_database.py
+++ DataTools/weez_database.py
@@ -1,9 +1,7 @@
 from AnalysisTools.weez_awards import WeezAwards
 from AnalysisTools.weez_reader import Player
-from dotenv import load_dotenv
-import pyrebase
-import os
-load_dotenv()
+from firebase_admin import credentials, firestore
+import firebase_admin
 
 
 class WeezDatabase:
@@ -11,35 +9,19 @@ class WeezDatabase:
     Object that handles data storage into Firebase.
     """
 
-    API_KEY = os.getenv('API_KEY')
-    AUTH_DOMAIN = os.getenv('AUTH_DOMAIN')
-    DATABASE_URL = os.getenv('DATABASE_URL')
-    PROJECT_ID = os.getenv('PROJECT_ID')
-    STORAGE_BUCKET = os.getenv('STORAGE_BUCKET')
-    MESSAGING_SENDER_ID = os.getenv('MESSAGING_SENDER_ID')
-    APP_ID = os.getenv('APP_ID')
-    MEASUREMENT_ID = os.getenv('MEASUREMENT_ID')
-
     def __init__(self):
+
         self._initialize_app()
-        self.db = self.firebase.database()
 
     def _initialize_app(self):
         """
-        Load the environment variables and initialise the connection to Firebase.
+        Get the credentials, initialise the app and create a connection to the Firestore database.
         :return: None.
         """
-        firebase_config = {
-            'apiKey': self.API_KEY,
-            'authDomain': self.AUTH_DOMAIN,
-            'databaseURL': self.DATABASE_URL,
-            'projectId': self.PROJECT_ID,
-            'storageBucket': self.STORAGE_BUCKET,
-            'messagingSenderId': self.MESSAGING_SENDER_ID,
-            'appId': self.APP_ID,
-            'measurementId': self.MEASUREMENT_ID,
-        }
-        self.firebase = pyrebase.initialize_app(firebase_config)
+        if not firebase_admin._apps:
+            cred = credentials.Certificate('/Users/rumeeahmed/Documents/Weez/DataTools/service_key.json')
+            firebase_admin.initialize_app(cred)
+        self.db = firestore.client()
 
     def add_games(self, player_list: list[Player]):
         """
@@ -48,11 +30,6 @@ class WeezDatabase:
         :return: None.
         """
         for player in player_list:
-            if player.gn:
-                gn = 'TRUE'
-            else:
-                gn = 'FALSE'
-
             game = {
                 'games_played': player.games_played,
                 'score': player.score,
@@ -65,9 +42,9 @@ class WeezDatabase:
                 'headshots': player.headshots,
                 'revives': player.revives,
                 'teams_wiped': player.teams_wiped,
-                'gn': gn,
+                'gn': player.gn,
             }
-            self.db.child('games').child(player.date).child(player.player_name).set(game)
+            self.db.collection('games').document(player.date).collection(player.player_name).document('stats').set(game)
 
     def add_awards(self, awards: WeezAwards):
         """
@@ -89,4 +66,4 @@ class WeezDatabase:
             'team_demolisher': awards.team_lover,
             'pussio': awards.pussio,
         }
-        self.db.child('awards').child(awards.date).set(data)
+        self.db.collection('awards').document(awards.date).set(data)
